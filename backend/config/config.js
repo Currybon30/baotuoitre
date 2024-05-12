3cbce24f94a20d275336dcb0c1573e6a7c8fec8a
Pham Tuong Nguyen <npham50@my.centennialcollege.ca>
2024-05-12 21:10:35 +0700

    update config.js

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,7 +1,7 @@
 import mongoCredentials from "../../credentials.js"
 const config = {
     env: 'development',
-    port: process.env.PORT || 5000,
+    port: process.env.PORT || 3000,
     jwtSecret: "YOUR_secret_key",
     mongoUri: mongoCredentials
   }
