76e9169eda4d677c43d4cfedbd1f4e01c5604973
Tuong Nguyen Pham <96371265+Currybon30@users.noreply.github.com>
2024-05-12 19:52:02 +0700

    Update config.js

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,7 +1,7 @@
 import mongoCredentials from "../../credentials.js"
 const config = {
     env: process.env.NODE_ENV || 'development',
-    port: process.env.PORT || 3000,
+    port: 3000,
     jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
     mongoUri: mongoCredentials || process.env.MONGODB_URI,
   }
