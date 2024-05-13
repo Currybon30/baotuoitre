ee8855dda572f3466a0eaa2de342d2374af69fcf
Pham Tuong Nguyen <npham50@my.centennialcollege.ca>
2024-05-13 08:34:30 +0700

    config

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,9 +1,10 @@
-import mongoCredentials from "../../credentials.js"
+import dotenv from 'dotenv'
+dotenv.config()
 const config = {
     env: 'development',
     port: 8000,
     jwtSecret: "YOUR_secret_key",
-    mongoUri: mongoCredentials
+    mongoUri: process.env.MONGODB_URI
   }
   
 export default config
