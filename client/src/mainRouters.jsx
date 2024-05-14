edaec43a23c04400bd1c698c1d9e1b56a42aaa9a
Pham Tuong Nguyen <npham50@my.centennialcollege.ca>
2024-05-14 10:41:30 +0700

    redirect pages

--- client/src/mainRouters.jsx
+++ client/src/mainRouters.jsx
@@ -1,6 +1,6 @@
 import Menu from './core/menu';
 import Footer from './core/footer';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import BieuMau from './home/bieumau';
 import Home from './home/home';
 import QuanLy from './manage/quanly';
@@ -8,17 +8,21 @@ import BieuMauOne from './manage/bieumauone';
 import ExportByMonth from './exports/exportByMonth';
 import ExportByPage from './exports/exportByPage';
 
+
 export default function MainRouters() {
     return (
         <div>
             <Menu />
             <Routes>
-                <Route exact path="/baotuoitre"  Component={Home}/>
-                <Route path="/baotuoitre/taobieumau" Component={BieuMau} />
-                <Route path="/baotuoitre/quanlybieumau" Component={QuanLy}  />
-                <Route path="/baotuoitre/quanlybieumau/:id" Component={BieuMauOne} />
-                <Route path="/baotuoitre/thongketheothang"  Component={ExportByMonth}/>
-                <Route path="/baotuoitre/thongketheotrang"  Component={ExportByPage}/>
+                <Route exact path="/"  Component={Home}/>
+                <Route path="/taobieumau" Component={BieuMau} />
+                <Route path="/quanlybieumau" Component={QuanLy}  />
+                <Route path="/quanlybieumau/:id" Component={BieuMauOne} />
+                <Route path="/thongketheothang"  Component={ExportByMonth}/>
+                <Route path="/thongketheotrang"  Component={ExportByPage}/>
+            
+                {/* New route for redirection */}
+                <Route path="/baotuoitre" element={<Navigate to="/" />} />
             </Routes>
             <Footer />  
         </div>
