9ffbf12d6e08b4f177d7a7270191e0828fa2fc4c
Pham Tuong Nguyen <96371265+Currybon30@users.noreply.github.com>
2024-06-12 15:53:40 +0700

    Update export.routes.js

--- backend/routes/export.routes.js
+++ backend/routes/export.routes.js
@@ -5,7 +5,6 @@ import exportCtrl from '../controllers/export.controllers.js';
 const router = express.Router();
 
 router.route('/api/export/exportByMonth/:year/:month')
-<<<<<<< server-demo
     .get(authCtrl.requireSignin, exportCtrl.exportToExcelByMonth);
 
 router.route('/api/export/exportByPage/:year/:month')
@@ -13,14 +12,5 @@ router.route('/api/export/exportByPage/:year/:month')
 
 router.route('/api/export/exportByDay/:year/:month/:day')
     .get(authCtrl.requireSignin, exportCtrl.exportToExcelByDay);
-=======
-    .get(exportCtrl.exportToExcelByMonth);
-
-router.route('/api/export/exportByPage/:year/:month')
-    .get(exportCtrl.exportToExcelByPage);
-
-router.route('/api/export/exportByDay/:year/:month/:day')
-    .get(exportCtrl.exportToExcelByDay);
->>>>>>> server
 
 export default router;
