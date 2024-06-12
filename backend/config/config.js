51ae9e309f3a1fa511814bee673ad208d2bacc22
Pham Tuong Nguyen <96371265+Currybon30@users.noreply.github.com>
2024-06-12 15:43:20 +0700

    Update config.js

--- backend/config/config.js
+++ backend/config/config.js
@@ -4,7 +4,7 @@ const config = {
     env: 'development',
     port: 8000,
     jwtSecret: "YOUR_secret_key",
-    mongoUri: 'mongodb://localhost:27017/QC', // process.env.MONGODB_URI
+    mongoUri: process.env.MONGODB_URI
   }
   
 export default config
