ce2d49944bed4e7c55a13a47a312784d6b95202e
Pham Tuong Nguyen <96371265+Currybon30@users.noreply.github.com>
2024-06-12 14:45:24 +0700

    authorization routes adjustment

--- backend/routes/export.routes.js
+++ backend/routes/export.routes.js
@@ -5,12 +5,12 @@ import authCtrl from '../controllers/auth.controllers.js';
 const router = express.Router();
 
 router.route('/api/export/exportByMonth/:year/:month')
-    .get(authCtrl.requireSignin, authCtrl.hasAuthorization, exportCtrl.exportToExcelByMonth);
+    .get(authCtrl.requireSignin, exportCtrl.exportToExcelByMonth);
 
 router.route('/api/export/exportByPage/:year/:month')
-    .get(authCtrl.requireSignin, authCtrl.hasAuthorization, exportCtrl.exportToExcelByPage);
+    .get(authCtrl.requireSignin, exportCtrl.exportToExcelByPage);
 
 router.route('/api/export/exportByDay/:year/:month/:day')
-    .get(authCtrl.requireSignin, authCtrl.hasAuthorization, exportCtrl.exportToExcelByDay);
+    .get(authCtrl.requireSignin, exportCtrl.exportToExcelByDay);
 
 export default router;
