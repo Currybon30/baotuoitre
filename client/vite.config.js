ee5a07cf6b6656df4ab89519f7accb73282c85e5
Tuong Nguyen Pham <96371265+Currybon30@users.noreply.github.com>
2024-08-17 19:43:40 -0400

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -26,6 +26,6 @@ export default defineConfig({
     rollupOptions: {
       input: './src/index.jsx',
     },
-    outDir: './client/dist', // Output directory for the build
+    outDir: './dist', // Output directory for the build
   },
 });
