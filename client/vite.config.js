bfd10bef33a14ad91d182371f7144c79731dc2e8
Tuong Nguyen Pham <96371265+Currybon30@users.noreply.github.com>
2024-08-17 19:30:38 -0400

    added files

--- client/vite.config.js
+++ client/vite.config.js
@@ -26,6 +26,6 @@ export default defineConfig({
     rollupOptions: {
       input: './src/index.jsx',
     },
-    outDir: '../public', // Output directory for the build
+    outDir: '../dist', // Output directory for the build
   },
 });
